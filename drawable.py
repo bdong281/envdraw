556f9999b0a4513728b66ddfe20fdf1d07e928e6
Dan Wang <d.wang1991@gmail.com>
2012-02-18 06:41:52 -0800

    Adding heuristic search for arrows
    --danwang

--- drawable.py
+++ drawable.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import math
+from heapq import heappush, heappop
+from functools import reduce
 
 class Drawable(object):
 
@@ -17,6 +19,14 @@ class Drawable(object):
         """
         return self.prefix + str(self.id)
 
+    def heuristic(self, point, goal):
+        distance = abs(point[0] - goal[0]) + abs(point[1] - goal[1])
+        x, y = point
+        covering = self.canvas.find_overlapping(x - 5, y - 5, x + 5, y + 5)
+        proximity = self.canvas.find_overlapping(x - 15, y - 15, x + 15, y +
+                15)
+        cost = len(covering) * 200 + len(proximity) * 150
+        return distance + cost
 
 # Connectables
 
@@ -44,11 +54,12 @@ class Draggable(Connectable):
         Connectable.__init__(self, canvas)
         self.canvas.tag_bind(self.tag, '<Button-1>', self.mouse_start)
         self.canvas.tag_bind(self.tag, '<B1-Motion>', self.mouse_drag)
+        self.canvas.tag_bind(self.tag, '<ButtonRelease-1>',
+                self.mouse_release)
         self._drag_x, self._drag_y= None, None
 
     def move(self, dx, dy):
         self.canvas.move(self.tag, dx, dy)
-        self.update_connectors()
 
     def mouse_start(self, event):
         self._drag_x, self._drag_y = event.x, event.y
@@ -57,6 +68,13 @@ class Draggable(Connectable):
         dx, dy = event.x - self._drag_x, event.y - self._drag_y
         self._drag_x, self._drag_y = event.x, event.y
         self.move(dx, dy)
+
+    def mouse_release(self, event):
+        coords = self.canvas.coords(self.tag)
+        x, y = coords[0], coords[1]
+        self.move(-(x % 10), -(y % 10))
+        self.update_connectors()
+
 
 
 class Frame(Draggable):
@@ -67,14 +85,15 @@ class Frame(Draggable):
         Draggable.__init__(self, canvas)
         self.variables = []
         self.values = []
-        self.rect = canvas.create_rectangle(x, y, x+150, y+35, tag=self.tag,
-                                            fill="white")
-        canvas.create_oval(x+135, y-15, x+165, y+15, tag=self.tag, fill="white")
+        self.rect = canvas.create_rectangle(x, y, x + self.width, y +
+                self.height, tag=self.tag, fill="white")
+        canvas.create_oval(x+135, y-15, x+165, y+15, tag=self.tag,
+                fill="white")
         if globe:
             pass
         else:
-            canvas.create_oval(x+145, y-5, x+155, y+5,
-                               tag=self.tag, fill="black")
+            canvas.create_oval(x+145, y-5, x+155, y+5, tag=self.tag,
+                    fill="black")
 
     @property
     def pos(self):
@@ -104,20 +123,39 @@ class Frame(Draggable):
         for value in self.values:
             value.move(dx, dy)
 
+    def mouse_release(self, event):
+        Draggable.mouse_release(self, event)
+        for variable in self.variables:
+            variable.update_connectors()
+        for value in self.values:
+            value.update_connectors()
+
     def update(self):
         x, y = self.pos
-        self.canvas.coords(self.rect, x, y, x+150, y+35+20*len(self.variables))
+        self.canvas.coords(self.rect, x, y, x + self.width, y + self.height)
 
     @property
     def inhandle(self):
         x, y = self.pos
-        return (x + 150, y + 15 + 20 * len(self.variables)), (x,
-                y + 15 + 20*len(self.variables))
+        print("INHANDLE frame: {0}".format((x + self.width, y + self.height), (x,
+            y + self.height)))
+        return (x + self.width, y + self.height - 20), (x, y + self.height -
+                20)
 
     @property
     def outhandle(self):
         x, y = self.pos
-        return x + 150, y
+        print("OUTHANDLE frame: {0}".format((x + self.width, y )))
+        return x + self.width, y
+
+
+    @property
+    def width(self):
+        return 150
+
+    @property
+    def height(self):
+        return 40 + 20 * len(self.variables)
 
 
 class Function(Draggable):
@@ -126,14 +164,13 @@ class Function(Draggable):
 
     def __init__(self, canvas, x, y, name, arguments, body="..."):
         Draggable.__init__(self, canvas)
-        self.shape = canvas.create_polygon(x, y, x+140, y, x+140, y+30, x+150,
-                                           y+30, x+150, y+60, x+10, y+60, x+10,
-                                           y+30, x, y+30, tag=self.tag,
-                                           fill="white", outline="white")
+        self.shape = canvas.create_polygon(x, y, x+140, y, x+140, y+30,
+                x+150, y+30, x+150, y+60, x+10, y+60, x+10, y+30, x, y+30,
+                tag=self.tag, fill="white", outline="white")
         canvas.create_line(x, y, x+140, y, x+140, y+30, x+150, y+30,
-                           tag=self.tag)
+                tag=self.tag)
         canvas.create_line(x, y+30, x+10, y+30, x+10, y+60, x+150, y+60,
-                           tag=self.tag)
+                tag=self.tag)
         canvas.create_oval(x+125, y-15, x+155, y+15, tag=self.tag, fill="white")
         canvas.create_oval(x+135, y-5, x+145, y+5, tag=self.tag, fill="black")
         self.name = canvas.create_text(x, y+5, tag=self.tag, anchor=tk.NW,
@@ -147,6 +184,7 @@ class Function(Draggable):
 
     @property
     def inhandle(self):
+        print("INHANDLE function: {0}".format((self.pos,)))
         return (self.pos,)
 
 
@@ -158,30 +196,32 @@ class Variable(Connectable):
         Connectable.__init__(self, canvas)
         x, y = frame.add_variable(self)
         self.text = canvas.create_text(x, y, anchor=tk.NW, text=name+":",
-                                       tag=self.tag)
+                tag=self.tag)
 
     def move(self, dx, dy):
         self.canvas.move(self.tag, dx, dy)
-        self.update_connectors()
 
     @property
     def pos(self):
+        print(tuple(self.canvas.coords(self.text)[0:2]))
         return tuple(self.canvas.coords(self.text)[0:2])
 
     @property
     def width(self):
         x1, _, x2, _ = self.canvas.bbox(self.text)
-        return x2 - x1
+        return ((x2 - x1)//10)*10
 
     @property
     def height(self):
         _, y1, _, y2 = self.canvas.bbox(self.text)
-        return y2 - y1
+        return ((y2 - y1)//10)*10
 
     @property
     def outhandle(self):
         x, y = self.pos
-        return x + self.width, y + self.height // 2
+        print("OUTHANDLE var: {0}".format((x + self.width, y +
+            self.height)))
+        return round(x + self.width, -1), round(y + self.height // 2, -1)
 
 
 class Value(Connectable):
@@ -196,7 +236,7 @@ class Value(Connectable):
 
     def move(self, dx, dy):
         self.canvas.move(self.tag, dx, dy)
-        self.update_connectors()
+#        self.update_connectors()
 
     @property
     def pos(self):
@@ -215,7 +255,7 @@ class Value(Connectable):
     @property
     def inhandle(self):
         x, y = self.pos
-        return (x - self.width, y + self.height // 2),
+        return (round(x - self.width, -1), round(y + self.height // 2, -1)),
 
 
 # Connector
@@ -233,27 +273,69 @@ class Connector(Drawable):
         self.head.add_connector(self)
         self.tail = tail
         self.tail.add_connector(self)
-        canvas.create_line(*self.gen_coords(), tag=self.tag)
+        self.line = self.gen_coords(update=True)
+        canvas.create_line(*self.line, tag=self.tag)
         self.arrow = Arrowhead(canvas, self)
-                
+
     def update(self):
         """
         Redraws this Connector based on the new position of its head and tail.
         """
-        self.canvas.coords(self.tag, *self.gen_coords())
+        self.canvas.coords(self.tag, *self.gen_coords(update=True))
         self.arrow.update()
 
     def closest_inhandle(self):
         return min(self.head.inhandle, key=lambda pt: \
                 self.distance(self.tail.outhandle, pt))
-        
+
     def distance(self, point1, point2):
         (x1, y1), (x2, y2) = point1, point2
         return abs(x1 - x2) + abs(y1 - y2)
 
-    def gen_coords(self):
-        (x1, y1), (x2, y2) = self.closest_inhandle(), self.tail.outhandle
-        return x1, y1, x2, y1, x2, y2
+    def gen_coords(self, update=False):
+        """
+        Generates a sequence of coordinates for drawing an arrow
+        """
+        if update:
+            start, goal = self.tail.outhandle, self.closest_inhandle()
+            closed, fringe = set(), []
+            initial = (start, [start])
+            heappush(fringe, (0, initial))
+
+            while fringe:
+                cost, (state, path) = heappop(fringe)
+                print(cost)
+                if state == goal:
+                    if len(path) == 1:
+                        path = path + path
+                    self.line = reduce(lambda x, y: x + y, path, ())
+                    return self.line
+
+                if state not in closed:
+                    closed.add(state)
+                    for direction in [(10, 0), (-10, 0), (0, 10), (0, -10)]:
+                        point = (state[0] + direction[0], state[1] + direction[1])
+                        new_cost = cost + self.heuristic(point, goal)
+                        if len(path) > 1:
+                            (x1, y1), (x2, y2) = path[0], path[1]
+                            if point[0] == x1 == x2 or point[1] == y1 == y2:
+                                new_path = [point] + path[1:]
+                            else:
+                                new_path = [point] + path
+                                prev_point = new_path[1]
+                                if point[1] == prev_point[1]:
+                                    dist = point[0] - prev_point[0]
+                                else:
+                                    dist = point[1] - prev_point[1]
+                                if dist < 2:
+                                    new_cost += 100
+                                new_cost += 50
+                        else:
+                            new_path = [point] + path
+                        heappush(fringe, (new_cost, (point, new_path)))
+            return None
+        else:
+            return self.line
 
 
 class Arrowhead(Drawable):
@@ -282,7 +364,7 @@ class Arrowhead(Drawable):
         x1, y1, x2, y2 = self.conn.gen_coords()[0:4]
         result = math.atan2(y1 - y2, x1 - x2)
         return result
-        
+
     def gen_coords(self, x, y, angle):
         pts = []
         for pt_x, pt_y in Arrowhead.points:
@@ -299,7 +381,7 @@ class Arrowhead(Drawable):
 
 if __name__ == '__main__':
     master = tk.Tk()
-    canvas = tk.Canvas(master)
+    canvas = tk.Canvas(master, width=600, height=600)
     canvas.pack(fill=tk.BOTH, expand=1)
     f = Frame(canvas, 100, 100, globe=True)
     foo = Variable(canvas, f, "foo")
