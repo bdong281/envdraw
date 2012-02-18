55c4b272ba0b97a5aa40ced168456fa597b33f70
Your Name <bdong281@gmail.com>
2012-02-18 07:23:53 -0800

    placing elements randomly

--- envdraw.py
+++ envdraw.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from drawable import *
 import ast
 from rewrite import *
+import random
 
 GLOBAL_FRAME = inspect.currentframe()
 
@@ -19,10 +20,10 @@ def test(f):
     return f
 
 class EnvDraw(object):
-    
+
     def __init__(self):
         self.master = tk.Tk()
-        self.canvas = tk.Canvas(self.master)
+        self.canvas = tk.Canvas(self.master, width=800, height=600)
         self.canvas.pack(fill=tk.BOTH, expand=1)
         self.tracker = Tracker(self, GLOBAL_FRAME)
         self.frame_tk = {}
@@ -34,12 +35,13 @@ class EnvDraw(object):
         self.canvas.delete(tk.ALL)
         frames = self.tracker.frames
         for frame in frames:
-            f = Frame(self.canvas, 100, 100)
+            x, y = self.place()
+            f = Frame(self.canvas, x, y)
             self.frame_tk[frame] = f
             for var, val in frames[frame].items():
                 variable_draw = Variable(self.canvas, f, var)
                 if type(val) == FUNCTION_TYPE:
-                    value_draw = Function(self.canvas, 200, 200, 
+                    value_draw = Function(self.canvas, 200, 200,
                         val.__name__, inspect.getargspec(val).args)
                     self.function_tk[val] = value_draw
                 else:
@@ -55,7 +57,19 @@ class EnvDraw(object):
             fn_tk = self.function_tk[fn]
             fr_tk = self.frame_tk[fr]
             Connector(self.canvas, fr_tk, fn_tk)
-            
+
+    def place(self):
+        x, y = random.randint(50, 650), random.randint(50, 500)
+        x, y = x//10*10, y//10*10
+        attempts = 0
+        while len(self.canvas.find_overlapping(x-10, y-10, x+160, y+80)) > 0:
+            if attempts > 30:
+                break
+            x, y = random.randint(50, 650), random.randint(50, 500)
+            x, y = x//10*10, y//10*10
+            attempts += 1
+        return x, y
+
 test.envdraw = EnvDraw()
 #exec(compile(open("test.py").read(), "test.py", 'exec'))
 if __name__ == "__main__":
