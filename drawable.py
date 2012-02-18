c9d97cc027b1db22886e982e6c3ad36b4bc75e5e
Eric Tzeng <eric.tzeng@Berkeley.edu>
2012-02-17 18:58:54 -0800

    [drawable] More interface design. Still really basic.

--- drawable.py
+++ drawable.py
@@ -2,17 +2,32 @@
 class Drawable(object):
     pass
 
+
 # Connectables
 
 class Connectable(Drawable):
-    pass
+
+    @property
+    def pos(self):
+        raise NotImplementedError
+
 
 class Frame(Connectable):
     pass
 
+
 class Function(Connectable):
     pass
+
 
 class Variable(Connectable):
     pass
 
+
+
+# Connector
+
+class Connector(Drawable):
+
+    def __init__(self, head, tail):
+        pass
