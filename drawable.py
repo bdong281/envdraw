a419b8e150d42975f612dce1f54cde9f26c7afa3
Eric Tzeng <eric.tzeng@Berkeley.edu>
2012-02-17 19:35:32 -0800

    [drawable] More interface stuff, and added very basic drawing for Frames.

--- drawable.py
+++ drawable.py
@@ -1,10 +1,13 @@
+import tkinter as tk
+
 class Drawable(object):
 
     _next_id = 0
 
-    def __init__(self):
+    def __init__(self, canvas):
         self.id = Drawable._next_id
         Drawable._next_id += 1
+        self.canvas = canvas
 
     @property
     def tag(self):
@@ -18,14 +21,26 @@ class Drawable(object):
 
 class Connectable(Drawable):
 
+    def __init__(self, canvas):
+        Drawable.__init__(self, canvas)
+        self.connectors = []
+
     @property
     def pos(self):
         raise NotImplementedError
+
+    def update_connectors(self):
+        for connector in self.connectors:
+            connector.update()
 
 
 class Frame(Connectable):
 
     prefix = "frame"
+
+    def __init__(self, canvas, x, y):
+        Connectable.__init__(self, canvas)
+        self.rect = canvas.create_rectangle(x, y, x+150, y+200, tag=self.tag)
 
 
 class Function(Connectable):
@@ -56,3 +71,10 @@ class Connector(Drawable):
         Redraws this Connector based on the new position of its head and tail.
         """
         pass
+
+if __name__ == '__main__':
+    master = tk.Tk()
+    canvas = tk.Canvas(master)
+    canvas.pack(fill=tk.BOTH, expand=1)
+    Frame(canvas, 100, 100)
+    tk.mainloop()
