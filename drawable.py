c0b53b89232acdba08f587c9f25f78a996d8758e
Eric Tzeng <eric.tzeng@Berkeley.edu>
2012-02-17 18:50:23 -0800

    Initial commit. Very basic framework code.

---
+++ drawable.py
@@ -0,0 +1,18 @@
+
+class Drawable(object):
+    pass
+
+# Connectables
+
+class Connectable(Drawable):
+    pass
+
+class Frame(Connectable):
+    pass
+
+class Function(Connectable):
+    pass
+
+class Variable(Connectable):
+    pass
+
