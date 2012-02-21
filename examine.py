6053e02e01cdf34ea0e03e06b767d1cb847ed71f
Tom Magrino <tmagrino@berkeley.edu>
2012-02-20 16:09:46 -0800

    Made examine.py "backwards compatible" for static files.

--- examine.py
+++ examine.py
@@ -186,10 +186,4 @@ if __name__ == '__main__':
     tree = ast.parse(open(sys.argv[1]).read())
     new_tree = ast.fix_missing_locations(AddFuncCall().visit(AddFuncReturn().visit(AddFuncDef().visit(tree))))
     exec(compile(new_tree, '<unknown>', 'exec'))
-
-    debug_pprint([v.variables for v in TRACKER.frames])
-    debug_print()
-    debug_pprint(TRACKER.frames)
-
-    TRACKER.draw()
     input()
